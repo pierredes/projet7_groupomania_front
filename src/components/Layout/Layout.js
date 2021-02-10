b72921a58a3031f5208d2685d083eb5d290bd7ea
pierredes <pierredesombre.pro@gmail.com>
2021-02-10 17:37:40 +0100

    add Layout  component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+import Auxiliaire from '../../hoc/Auxiliaire/Auxiliaire';
+
+import './Layout.css';
+
+const layout = (props) => {
+    return (
+        <Auxiliaire>
+            <Header />
+            <main>
+                
+            </main>
+        </Auxiliaire>
+    )
+}
+
+export default layout;
