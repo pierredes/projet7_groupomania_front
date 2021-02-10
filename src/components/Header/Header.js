837302c7c6ebc1c7e6999f1b84c9050bef7059fd
pierredes <pierredesombre.pro@gmail.com>
2021-02-10 17:22:46 +0100

    add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './header.css';
+
+const header = () => (
+        <header>
+            <h1> Groupomania</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                    <li><a href="/">Créer un post</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+
+export default header;
