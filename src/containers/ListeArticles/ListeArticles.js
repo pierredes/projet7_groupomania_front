0674c64d2069b4eeeb043e7971c8a198ec612ec1
pierredes <pierredesombre.pro@gmail.com>
2021-02-10 19:19:40 +0100

    add list of post container with axios method

---
+++ src/containers/ListeArticles/ListeArticles.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Article from '../../components/Article/Article';
+
+import './ListeArticles.css';
+
+class ListeArticles extends Component {
+
+    state = {
+        articles: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/api/post')
+            .then(res => {
+                const postLimite = res.data.post.slice(0,10);
+                const listeArticle = postLimite.map(article => {
+                    return {
+                        ...article
+                    };
+                })
+                this.setState({ articles : listeArticle})
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+    
+    render() {
+        return (
+            <div className="listeArticle">
+                <h2> test</h2>
+                <Article article={this.state.articles} />
+            </div>
+            
+        )
+    }
+
+
+
+}
+
+export default ListeArticles;
