5d388e6310a74b479427bcaa1700ac8b5b303429
pierredes <pierredesombre.pro@gmail.com>
2021-02-12 13:08:18 +0100

    add route logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Layout from './components/Layout/Layout';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import ListeArticles from './containers/ListeArticles/ListeArticles'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      
+      <Layout />
+        <Switch>
+          {/* <Route path='/creer-post' component={ListeArticles}/> */}
+          <Route path='/' exact component={ListeArticles} />
+        </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
