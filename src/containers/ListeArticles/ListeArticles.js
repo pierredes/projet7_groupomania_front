bbadfc24e2e0e086607cb0797452d8f5f2a65e93
pierredes <pierredesombre.pro@gmail.com>
2021-02-24 16:15:09 +0100

    add authentificate

--- src/containers/ListeArticles/ListeArticles.js
+++ src/containers/ListeArticles/ListeArticles.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import Article from '../../components/Article/Article';
 
 import './ListeArticles.css';
+import ArticleComplet from '../ArticleComplet/ArticleComplet';
 
 class ListeArticles extends Component {
 
     state = {
-        articles: []
+        articles: [],
+        selectedPostId: null,
+        connecter: false
     }
 
-    componentDidMount() {
+    componentDidMount(props) {
         axios.get('http://localhost:3000/api/post')
             .then(res => {
                 const postLimite = res.data.post.slice(0,10);
@@ -25,6 +29,10 @@ class ListeArticles extends Component {
             .catch(error => {
                 console.log(error)
             });
+            const token = localStorage.getItem('token');
+        if(token) {
+            this.setState({connecter: true})
+        }
     }
 
     postSelectedHandler = (id) => {
@@ -32,21 +40,35 @@ class ListeArticles extends Component {
     }
 
     render() {
-        let posts = this.state.articles.map(article => {
-            return (
-                <Article 
-                    titre={article.titre}
-                    sujet={article.sujet}
-                    contenu={article.contenu}
-                    key={article.id} 
-                    click={() => this.postSelectedHandler(article.id)} />
+        let posts = null;
+
+        if(this.state.connecter){
+            posts = this.state.articles.map(article => {
+                return (
+                    <Link to={'/' + article.id} key={article.id}>
+                        <Article 
+                            titre={article.titre}
+                            sujet={article.sujet}
+                            contenu={article.contenu.substring(0,300)} 
+                            click={() => this.postSelectedHandler(article.id)} />
+                    </Link>
+                )
+            })
+        }
+        else {
+            posts =  (
+                <p> Pour voir le contenu de cette page veuilez vous connecter</p>
             )
-        })
+        }
+
+        
 
         return (
-            <div className="listeArticle">
+            <div>
+                <section className="listeArticle">
                 <h2> test</h2>
                 {posts}
+                </section>
             </div>
             
         )
