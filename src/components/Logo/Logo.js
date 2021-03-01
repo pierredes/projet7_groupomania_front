a75ab0e2732a142d664e61567152de906ae2044d
pierredes <pierredesombre.pro@gmail.com>
2021-03-01 17:02:16 +0100

    add logo component with his css

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Logo from '../../assets/image.png';
+
+const logo = (props) => (
+    <div className="logo">
+        <img src={Logo} alt="Groupomania" height='300' />
+    </div>
+)
+
+export default logo;
