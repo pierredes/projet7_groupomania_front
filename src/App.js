fee018f049b52ae972aaa32b190dcd33c4dcae14
pierredes <pierredesombre.pro@gmail.com>
2021-03-02 09:11:39 +0100

    add all connect logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
 import './App.css';
-import Layout from './components/Layout/Layout';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from './hoc/Layout/Layout';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import ListeArticles from './containers/ListeArticles/ListeArticles'
+import ListeArticles from './containers/ListeArticles/ListeArticles';
+import ArticleComplet from './containers/ArticleComplet/ArticleComplet';
+import CreerArticle from './containers/CreerArticle/CreerArticle';
+import ModifierArticle from './containers/ModifierArticle/ModifierArticle';
+import inscription from './containers/Inscription/Inscription';
+import Connection from './containers/Connection/Connection';
+import ConnecterContext from './Context/Context';
 
-function App() {
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <Layout />
-        <Switch>
-          {/* <Route path='/creer-post' component={ListeArticles}/> */}
-          <Route path='/' exact component={ListeArticles} />
-        </Switch>
-    </div>
-    </BrowserRouter>
-  );
+
+class App extends Component {
+
+  state = {
+    redirection: false,
+    connecter: false,
+    token: null,
+    redirection: false
+  }
+
+  connection = (email, password) => {
+    let data = {
+        email: Object.values(email)[0],
+        password: Object.values(password)[0]
+    }
+    axios.post('http://localhost:3000/api/authentification/connection/', data)
+        .then(res => {
+            const token = res.data.token.split('.')[1];
+            localStorage.setItem('token', token);
+            this.setState({ redirection: true, connecter: true, token: token});
+        })
+        .catch(err => {
+            console.log(err)
+        })
+  }
+
+  deconnection = () => {
+    this.setState({ connecter: false, token: null, redirection: false})
+    localStorage.removeItem('token');
+}
+
+
+  componentDidMount() {
+    if(localStorage.getItem('token')) {
+      this.setState({connecter: true, token: localStorage.getItem('token')})
+    }
+  }
+  render () {
+
+    return (
+      <BrowserRouter>
+      <ConnecterContext.Provider value={{connecter: this.state.connecter, token: this.state.token}}>
+      <Layout deconnection={this.deconnection}>
+      <div className="App">
+        
+          <Switch>
+            <Route path='/inscription' component={inscription} />
+            <Route path='/connection' render={(props) => (<Connection connection={this.connection} redirection={this.state.redirection} />)} />
+            <Route path='/creer-post' component={CreerArticle}/>
+            <Route path='/' exact component={ListeArticles} />
+            <Route path='/:id' exact component={ArticleComplet} />
+            <Route path='/modifier/:id' exact component={ModifierArticle} />
+            
+          </Switch>
+      </div>
+      </Layout>
+      </ConnecterContext.Provider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
