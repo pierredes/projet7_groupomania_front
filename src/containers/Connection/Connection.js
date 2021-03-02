31e6ae1f99de185be242ecb8087e5f4d10321193
pierredes <pierredesombre.pro@gmail.com>
2021-03-02 09:33:24 +0100

    remove all connect logic (move them to app)'

--- src/containers/Connection/Connection.js
+++ src/containers/Connection/Connection.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component} from 'react';
 import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/Input/Input';
@@ -8,41 +7,28 @@ class connection extends Component {
 
     state = {
             email: '',
-            password: '',
-            redirection: false,
-            connecter: false,
-            token: null
+            password: ''
     }
-
-
-    connection = (event) => {
-        event.preventDefault();
-        let data = {
-            email: this.state.email,
-            password: this.state.password,
-        }
-        axios.post('http://localhost:3000/api/authentification/connection/', data)
-            .then(res => {
-                const token = res.data.token.split('.')[1];
-                localStorage.setItem('token', token);
-                this.setState({ redirection: true, connecter: true, token: token  } )
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
 
     render() {
 
-        if(this.state.redirection) {
-            return <Redirect to={{pathName: '/', state: { connecter: this.state.connecter, token: this.state.token}}}  />
+        if(this.props.redirection) {
+            return <Redirect to="/" />
         }
+
+        let email = {email: this.state.email}
+        let password = {password: this.state.password}
+
         return (
             <form>
                 <Input inputtype='input' type="email" name="email" placeholder="email" value={this.state.email} onChange={(event) => {this.setState({ email: event.target.value })}} required/>
                 <Input inputtype='input' type="password" name="password" placeholder="mot de passe" value={this.state.password} onChange={(event) => {this.setState({ password: event.target.value })}} required/>
-                <button onClick={this.connection}> Envoyer </button>
+                <button onClick={
+                    (event) => {
+                        event.preventDefault()
+                        this.props.connection(email, password); 
+                    }
+                }> Envoyer </button>
             </form>
 
         )
