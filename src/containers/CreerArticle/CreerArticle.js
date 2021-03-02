90ecd79d83f6aeac59e41caeeae2caae6873f738
pierredes <pierredesombre.pro@gmail.com>
2021-03-02 09:31:51 +0100

    add message after post & redirection on js file change color on css

--- src/containers/CreerArticle/CreerArticle.js
+++ src/containers/CreerArticle/CreerArticle.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import Input from '../../components/Input/Input';
 import './CreerArticle.css'
@@ -9,10 +10,13 @@ class creerArticle extends Component {
     state = {
         titre: '',
         sujet: '',
-        contenu: ''
+        contenu: '',
+        redirection: false,
+        message: ''
     }
 
-    postData = () => {
+    postData = (event) => {
+        event.preventDefault();
         let data = {
             titre: this.state.titre,
             sujet: this.state.sujet,
@@ -20,7 +24,8 @@ class creerArticle extends Component {
         };
         axios.post('http://localhost:3000/api/post/', data)
             .then(res => {
-                return <p> Votre article a bien été crée</p>
+                // this.setState({ redirection: true })
+               this.setState({ message: res.data.message })
             })
             .catch(error => {
                 console.log(error)
@@ -29,11 +34,15 @@ class creerArticle extends Component {
 
     render() {
 
+        if(this.state.redirection) {
+            return <Redirect to='/' />
+        }
+
         let form = (
             <form>
-                <Input inputtype='input' type="text" name="titre" placeholder="titre" value={this.state.titre} onChange={(event) => {this.setState({ titre: event.target.value })}} required/>
-                <Input inputtype='input' type="text" name="sujet" placeholder="sujet" onChange={(event) => {this.setState({ sujet: event.target.value })}} />
-                <Input inputtype='textarea' type="textarea" name="Contenu" placeholder="Contenu" onChange={(event) => {this.setState({ contenu: event.target.value })}} />
+                <Input inputtype='input' type="text" name="titre" placeholder="titre" label='Veuillez renseigner un titre (ceci est obligatoire)' value={this.state.titre} onChange={(event) => {this.setState({ titre: event.target.value })}} required/>
+                <Input inputtype='input' type="text" name="sujet" placeholder="sujet" label='De quoi va parler votre article ? (ceci est obligatoire)' onChange={(event) => {this.setState({ sujet: event.target.value })}} />
+                <Input inputtype='textarea' type="textarea" name="Contenu" placeholder="Contenu" label='Dites nous en plus sur le sujet ! (ceci est obligatoire)' onChange={(event) => {this.setState({ contenu: event.target.value })}} />
                 <button onClick={this.postData}> Envoyer </button>
             </form>
         )
@@ -43,6 +52,7 @@ class creerArticle extends Component {
             <div>
                 <h1> Créer votre article !</h1>
                 {form}
+                {this.state.message}
             </div>
             
         )
