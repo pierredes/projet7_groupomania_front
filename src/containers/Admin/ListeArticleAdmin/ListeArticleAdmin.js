a7fa7bc036ab7976dfe3343479743828af7b1a30
pierredes <pierredesombre.pro@gmail.com>
2021-03-15 16:13:08 +0100

    Add posts administration

---
+++ src/containers/Admin/ListeArticleAdmin/ListeArticleAdmin.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import ConnecterContext from '../../../Context/Context';
+import ListeArticles from '../../ListeArticles/ListeArticles';
+import Style from './ListeArticleAdmin.module.css'
+
+class admin extends ListeArticles {
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+            updatePage: false
+        }
+    }
+
+    componentDidUpdate() {
+        if(this.state.updatePage) {
+            axios.get('http://localhost:3000/api/post/',  {headers: {Authorization: localStorage.getItem('token')}})
+            .then((res) => {
+                const posts = res.data.post;
+                const postsUpdate = posts.map(article => {
+                    return {
+                        ...article
+                    }
+                })
+                this.setState({ updatePage: false, articles: postsUpdate })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    }
+
+    supprimerArticle = (id) => {
+        axios.delete('http://localhost:3000/api/post/supression/' + id,  {headers: {Authorization: localStorage.getItem('token')}})
+            .then(() => {
+                this.setState({ updatePage: true });
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    render() {
+
+        let post = null;
+        post = this.state.articles.map((article) => {
+             return (
+                <Link to={'/' + article.id} key={article.id} className={Style.Article}>
+                <div>
+                    <h3>Titre : {article.titre}</h3>
+                    <p>Sujet : {article.sujet}</p>
+                    <p> De: {article.utilisateur.prenom} {article.utilisateur.nom}</p>
+                    <button onClick={() => {this.supprimerArticle(article.id)}}> Supprimer </button>
+                </div>
+                </Link>
+            )
+        })
+
+        return (
+            <ConnecterContext.Consumer>
+                {(context) => (
+                    context.admin ? 
+                        <div>
+                            <h1> Bienvenue sur l'interface d'administration</h1>
+                            <h2> Voici la liste des posts disponible !</h2>
+                            <p> Si vous souhaitez administrer les commentaires cliquez <Link to='/admin/commentaire/'>ici</Link></p>
+                            <div className={Style.ListeArticle}>
+                                {post}
+                            </div>
+                        </div>
+                    :
+                    <div>
+                        <h1> Veuillez vous connectez en tant qu'admin</h1>
+                    </div>
+                )}
+                
+            </ConnecterContext.Consumer>
+        )
+    }
+}
+    
+
+
+
+
+export default admin;
