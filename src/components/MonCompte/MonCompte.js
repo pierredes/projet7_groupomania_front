3e1c1ac540e8ce698afd0077bc41d8502939f6f6
pierredes <pierredesombre.pro@gmail.com>
2021-03-29 14:50:45 +0200

    Add disconnect on click (using props)

--- src/components/MonCompte/MonCompte.js
+++ src/components/MonCompte/MonCompte.js
@@ -6,17 +6,15 @@ import './MonCompte.css'
 const MonCompte = (props) => {
 
     const suppresionDeCompte = () => {
-        axios.delete('http://localhost:3000/api/user/supression/' + localStorage.getItem('userId'), {headers: {Authorization: localStorage.getItem('token')}})
+        axios.delete('http://localhost:8080/api/user/supression/' + localStorage.getItem('userId'), {headers: {Authorization: localStorage.getItem('token')}})
         .then(() => {
-            
         })
     }
-
 
    return (
        <div className="Conteneur">
            <div className="MonCompte">
-                <button onClick={suppresionDeCompte}> Supprimer mon compte </button>
+                <button onClick={() => {suppresionDeCompte(); props.supression()}}> Supprimer mon compte </button>
             </div>
             <div className="AVenir">
                 <p>Prochainement</p>
