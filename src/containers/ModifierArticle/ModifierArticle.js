e71b8116709a1cbb5d05145a0e9122d7d8df5983
pierredes <pierredesombre.pro@gmail.com>
2021-03-30 18:01:00 +0200

    add display error & data verification

--- src/containers/ModifierArticle/ModifierArticle.js
+++ src/containers/ModifierArticle/ModifierArticle.js
@@ -11,23 +11,43 @@ class modifierArticle extends Component {
         titre: this.props.location.params.titre,
         sujet: this.props.location.params.sujet,
         contenu: this.props.location.params.contenu,
+        error: '',
         redirection: false
+    }
+
+    verifier_donnees_article = (texte) => {
+        let regex_texte = /^[A-Za-z0-9éèêëç-\s]{2,100}$/;
+        if(regex_texte.test(texte) == false) {
+            return false;
+        }
+        else {
+            return texte;
+        }
     }
 
     postData = (event) => {
         event.preventDefault();
-        let data = {
-            titre: this.state.titre,
-            sujet: this.state.sujet,
-            contenu: this.state.contenu,
-        };
-        axios.put('http://localhost:3000/api/post/modification/' + this.props.match.params.id, data, {headers: {Authorization: localStorage.getItem('token')}})
-            .then(res => {
-                this.setState({ redirection: true });
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        if(this.verifier_donnees_article(this.state.titre)) {
+            if(this.verifier_donnees_article(this.state.sujet)) {
+                let data = {
+                    titre: this.state.titre,
+                    sujet: this.state.sujet,
+                    contenu: this.state.contenu,
+                    user_id: localStorage.getItem('userId')
+                };
+                axios.put('http://localhost:8080/api/post/modification/' + this.props.match.params.id, data, {headers: {Authorization: localStorage.getItem('token')}})
+                    .then(res => {
+                        this.setState({ redirection: true });
+                    })
+                    .catch(error => {
+                        this.setState({ error: error.response.data.message })
+                    })
+            } else {
+                this.setState({ error: 'veuillez saisir uniquement du texte d\'au moins 2 caractères pour le sujet de votre article' })
+            }
+        } else {
+            this.setState({ error: 'veuillez saisir uniquement du texte d\'au moins 2 caractères pour le titre de votre article' })
+        }
     }
 
     render() {
@@ -38,10 +58,11 @@ class modifierArticle extends Component {
 
         let form = (
             <form>
-                <Input inputtype='input' type="text" name="titre" placeholder="titre" label='Une erreur dans le titre ? vous pouvez le rectifier ! (ceci est obligatoire)' value={this.state.titre} onChange={(event) => {this.setState({ titre: event.target.value })}} required/>
-                <Input inputtype='input' type="text" name="sujet" placeholder="sujet" label='Une erreur dans le sujet ? vous pouvez le rectifier ! (ceci est obligatoire)' value={this.state.sujet} onChange={(event) => {this.setState({ sujet: event.target.value })}} />
-                <Input inputtype='textarea' type="textarea" name="Contenu" placeholder="Contenu"  label='Une erreur dans le contenu ? vous pouvez le rectifier ! (ceci est obligatoire)'value={this.state.contenu} onChange={(event) => {this.setState({ contenu: event.target.value })}} />
+                <Input inputtype='input' type="text" name="titre" placeholder="titre" label='Une erreur dans le titre ? vous pouvez le rectifier !' value={this.state.titre} onChange={(event) => {this.setState({ titre: event.target.value })}} required/>
+                <Input inputtype='input' type="text" name="sujet" placeholder="sujet" label='Une erreur dans le sujet ? vous pouvez le rectifier !' value={this.state.sujet} onChange={(event) => {this.setState({ sujet: event.target.value })}} />
+                <Input inputtype='textarea' type="textarea" name="Contenu" placeholder="Contenu"  label='Une erreur dans le contenu ? vous pouvez le rectifier !'value={this.state.contenu} onChange={(event) => {this.setState({ contenu: event.target.value })}} />
                 <button onClick={this.postData}> Envoyer </button>
+                <p className="message_validation">{this.state.error}</p>
             </form>
         )
 
