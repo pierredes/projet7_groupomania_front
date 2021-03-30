b8faa4860b8091cf0114e30d7e21fc8ff3df5603
pierredes <pierredesombre.pro@gmail.com>
2021-03-30 17:59:31 +0200

    add display error & data verification

--- src/containers/CreerArticle/CreerArticle.js
+++ src/containers/CreerArticle/CreerArticle.js
@@ -12,24 +12,43 @@ class creerArticle extends Component {
         sujet: '',
         contenu: '',
         redirection: false,
-        message: ''
+        message: '',
+        error: ''
+    }
+
+    verifier_donnees_article = (texte) => {
+        let regex_texte = /^[A-Za-z0-9éèêëç-\s]{2,100}$/;
+        if(regex_texte.test(texte) == false) {
+            return false;
+        }
+        else {
+            return texte;
+        }
     }
 
     postData = (event) => {
         event.preventDefault();
-        let data = {
-            titre: this.state.titre,
-            sujet: this.state.sujet,
-            contenu: this.state.contenu,
-            user_id: localStorage.getItem('userId')
-        };
-        axios.post('http://localhost:3000/api/post/', data, {headers: {Authorization: localStorage.getItem('token')}})
-            .then(res => {
-               this.setState({ message: res.data.message, titre: '', sujet: "", contenu: '' })
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        if(this.verifier_donnees_article(this.state.titre)) {
+            if(this.verifier_donnees_article(this.state.sujet)) {
+                let data = {
+                    titre: this.state.titre,
+                    sujet: this.state.sujet,
+                    contenu: this.state.contenu,
+                    user_id: localStorage.getItem('userId')
+                };
+                axios.post('http://localhost:8080/api/post/', data, {headers: {Authorization: localStorage.getItem('token')}})
+                    .then(res => {
+                       this.setState({ message: res.data.message, titre: '', sujet: "", contenu: '', error: '' })
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            } else {
+                this.setState({ error: 'veuillez saisir uniquement du texte d\'au moins 2 caractères pour le sujet de votre article' })
+            }
+        } else {
+            this.setState({ error: 'veuillez saisir uniquement du texte d\'au moins 2 caractères pour le titre de votre article' })
+        }
     }
 
     render() {
@@ -44,6 +63,7 @@ class creerArticle extends Component {
                 <Input inputtype='input' type="text" name="sujet" placeholder="sujet" label='De quoi va parler votre article ?' value={this.state.sujet} onChange={(event) => {this.setState({ sujet: event.target.value })}} />
                 <Input inputtype='textarea' type="textarea" name="Contenu" placeholder="Contenu" label='Dites nous en plus sur le sujet !' value={this.state.contenu} onChange={(event) => {this.setState({ contenu: event.target.value })}} />
                 <button onClick={this.postData}> Envoyer </button>
+                <p className="message_validation">{this.state.error}</p>
             </form>
         )
 
