fae6a49ff5a21ce98b4bc3add7577fe2b9023c2f
pierredes <pierredesombre.pro@gmail.com>
2021-03-30 17:56:28 +0200

    add context admin verification & suppress option

--- src/containers/Admin/ListeCommentaireAdmin/ListeCommentaireAdmin.js
+++ src/containers/Admin/ListeCommentaireAdmin/ListeCommentaireAdmin.js
@@ -1,5 +1,6 @@
 import react, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import ConnecterContext from '../../../Context/Context';
 
@@ -8,11 +9,14 @@ import Style from './ListeCommentaireAdmin.module.css';
 class ListeCommentaireAdmin extends Component {
 
     state = {
-        commentaires: []
+        commentaires: [],
+        updatePage: false
     }
 
+    static contextType = ConnecterContext;
+
     componentDidMount() {
-        axios.get('http://localhost:3000/api/commentaire/', {headers: {Authorization: localStorage.getItem('token')}})
+        axios.get('http://localhost:8080/api/commentaire/', {headers: {Authorization: localStorage.getItem('token')}})
         .then(res => {
             const commentaire = res.data.commentaire;
             const recupererTousLesCommentaire = commentaire.map(commentaire => {
@@ -24,13 +28,41 @@ class ListeCommentaireAdmin extends Component {
         })
     }
 
+    componentDidUpdate() {
+        if(this.state.updatePage) {
+            axios.get('http://localhost:8080/api/commentaire/',  {headers: {Authorization: localStorage.getItem('token')}})
+            .then((res) => {
+                const commentaire = res.data.commentaire;
+                const commentaireUpdate = commentaire.map(commentaire => {
+                    return {
+                        ...commentaire
+                    }
+                })
+                this.setState({ updatePage: false, commentaires: commentaireUpdate })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    }
+
+    supprimerCommentaire = (id) => {
+        axios.delete('http://localhost:8080/api/commentaire/supprimer/' + id,  {data: {admin: this.context.admin}, headers: {Authorization: localStorage.getItem('token')}})
+            .then(() => {
+                this.setState({ updatePage: true });
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     render() {
         let commentaire = null;
         commentaire = this.state.commentaires.map(commentaire => {
             return (
                 <div key={commentaire.id} className={Style.Commentaire}>
                     <p>{commentaire.contenu}</p>
-                    <button> Supprimer</button>
+                    <button onClick={() => this.supprimerCommentaire(commentaire.id)}> Supprimer</button>
                 </div>
             )
         })
@@ -38,8 +70,10 @@ class ListeCommentaireAdmin extends Component {
             <ConnecterContext.Consumer>
                 {(context) => (
                     context.admin ?
-                        <div>       
-                            <h1> Voici la pages d'administration des commentaire</h1>
+                        <div>
+                            <h1> Bienvenue sur l'interface d'administration</h1>
+                            <h2> Voici la pages d'administration des commentaire</h2>
+                            <button><Link to='/admin/'>Article</Link></button>
                             <div className={Style.ListeCommentaire}>
                                 {commentaire}
                             </div>
